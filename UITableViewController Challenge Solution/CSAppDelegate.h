20b0f4cf6d8f73fef693524659a9daf02ad922f3
jdrsqrd <jdrsqrd@gmail.com>
2014-03-01 14:26:27 -0500

    Initial Commit

---
+++ UITableViewController Challenge Solution/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Johnathan Ross on 3/1/14.
+//  Copyright (c) 2014 JDR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CSViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Johnathan Ross on 3/1/14.
+//  Copyright (c) 2014 JDR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSViewController : UIViewController
+
+@end
