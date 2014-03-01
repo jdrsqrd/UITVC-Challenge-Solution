efe1c2cd134fb942d5076d962aa2ee2e0fbf2c1b
jdrsqrd <jdrsqrd@gmail.com>
2014-03-01 14:55:18 -0500

    Fully complete challenge

--- UITableViewController Challenge Solution/CSTableViewController.m
+++ UITableViewController Challenge Solution/CSTableViewController.m
@@ -44,15 +44,16 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
+    if (section==0) {
+        return 2;
+    }else if (section==1) return 1;
+    else if (section == 2) return 3;
     return 0;
 }
 
@@ -62,6 +63,18 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
     // Configure the cell...
+    
+    if (indexPath.section==0) {
+        cell.textLabel.textColor = [UIColor redColor];
+        cell.textLabel.text = [NSString stringWithFormat: @"I am in section %i",indexPath.section];
+    }else if (indexPath.section==1){
+        cell.textLabel.textColor = [UIColor blueColor];
+        cell.textLabel.text = @"Another Section";
+    }else if (indexPath.section == 2){
+        cell.textLabel.textColor = [UIColor yellowColor];
+        cell.textLabel.text = [NSString stringWithFormat: @"Cell row %i",indexPath.row];
+    }
+    
     
     return cell;
 }
